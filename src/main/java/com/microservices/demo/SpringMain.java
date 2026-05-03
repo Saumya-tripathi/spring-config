315f48dcfefc05e3f244daa7fbb582c91eba7e53
Saumya-tripathi <psitsaumya@gmail.com>
2026-05-03 20:58:21 +0530

    first commit

--- src/main/java/com/microservices/demo/SpringMain.java
+++ src/main/java/com/microservices/demo/SpringMain.java
@@ -1,4 +1,14 @@
 package com.microservices.demo;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
 public class SpringMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMain.class, args);
+    }
 }
